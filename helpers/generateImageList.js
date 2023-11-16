190f2af306ce367c4dbca11aedd1fe465e3e37bc
Ricardo Behrens <behrensricardo@gmail.com>
2023-11-16 12:09:18 -0700

    Still playing around with the image grid

--- helpers/generateImageList.js
+++ helpers/generateImageList.js
@@ -1,8 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 
-const publicPath = path.join(__dirname, '../public');
-const imagesPath = path.join(publicPath, 'images');
+const projectRoot = path.join(__dirname, '..'); // Assuming the script is in the helpers folder
+const srcPath = path.join(projectRoot, 'src');
+const assetsPath = path.join(srcPath, 'assets');
+const imagesPath = path.join(assetsPath, 'images');
 
 function getAllImages(directory) {
   const files = fs.readdirSync(directory);
@@ -14,6 +16,7 @@ const imagesList = getAllImages(imagesPath);
 
 const jsonContent = JSON.stringify(imagesList, null, 2);
 
-fs.writeFileSync(path.join(publicPath, 'images.json'), jsonContent);
+const jsonFilePath = path.join(assetsPath, 'images.json');
+fs.writeFileSync(jsonFilePath, jsonContent);
 
-console.log('Image list JSON file generated successfully.');
+console.log(`Image list JSON file generated successfully at: ${jsonFilePath}`);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
+import images from '../assets/images.json'
 
 export default function Home() {
-  const [imageList, setImageList] = useState();
+  const [imageList, setImageList] = useState(images);
   
   return (
     <div>
       <h1>Home Page</h1>
+      <div className='image-grid'>
+        {imageList.map((image) => <img src={image} alt={image.public_id}></img>)}
+      </div>
   
     </div>
   );
