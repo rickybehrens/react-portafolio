ad40fffca11f2fba1f5ac9987b2ff00d0786beb2
Ricardo Behrens <behrensricardo@gmail.com>
2023-11-16 17:16:46 -0700

    Added the pictures and the scrip to create the images.json file succesfully. I now need to add my protafolio and hook up the contact page to something that's relevant and good for me

--- helpers/generateImageList.js
+++ helpers/generateImageList.js
@@ -2,21 +2,20 @@ const fs = require('fs');
 const path = require('path');
 
 const projectRoot = path.join(__dirname, '..'); // Assuming the script is in the helpers folder
-const srcPath = path.join(projectRoot, 'src');
-const assetsPath = path.join(srcPath, 'assets');
-const imagesPath = path.join(assetsPath, 'images');
+const publicPath = path.join(projectRoot, 'public');
+const imagesPath = path.join(publicPath, 'images');
 
 function getAllImages(directory) {
   const files = fs.readdirSync(directory);
   const imageFiles = files.filter(file => /\.(jpg|jpeg|png|gif|bmp)$/i.test(file));
-  return imageFiles.map(file => path.join(directory, file));
+  return imageFiles.map(file => path.relative(publicPath, path.join(directory, file)));
 }
 
 const imagesList = getAllImages(imagesPath);
 
 const jsonContent = JSON.stringify(imagesList, null, 2);
 
-const jsonFilePath = path.join(assetsPath, 'images.json');
+const jsonFilePath = path.join(publicPath, 'images.json');
 fs.writeFileSync(jsonFilePath, jsonContent);
 
 console.log(`Image list JSON file generated successfully at: ${jsonFilePath}`);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,31 @@
-import React, { useState } from 'react';
-import images from '../assets/images.json'
+import React, { useState, useEffect } from 'react';
 
 export default function Home() {
-  const [imageList, setImageList] = useState(images);
-  
+  const [imageList, setImageList] = useState([]);
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      try {
+        const response = await fetch('/images.json');
+        const data = await response.json();
+        setImageList(data);
+      } catch (error) {
+        console.error('Error fetching images:', error);
+      }
+    };
+
+    fetchImages();
+  }, []);
+
   return (
     <div>
       <h1>Home Page</h1>
+      <h4>A picture is worth a thousand words!</h4>
       <div className='image-grid'>
-        {imageList.map((image) => <img src={image} alt={image.public_id}></img>)}
+        {imageList.map((image, index) => (
+          <img key={index} src={`${image}`} alt={`image-${index}`} />
+        ))}
       </div>
-  
     </div>
   );
 }
