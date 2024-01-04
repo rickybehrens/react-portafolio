b2891095cd7921e27cf08a1f9292beedbafcd529
Ricardo Behrens <behrensricardo@gmail.com>
2024-01-04 14:49:45 -0700

    Created the website and I have been working on getting my contact form to work using EmailJS services but cant get the form to work properly

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,73 @@
-import React from 'react';
+import React, { useState, useRef } from "react";
+import emailjs from '@emailjs/browser';
 
 export default function Contact() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const [touchedFields, setTouchedFields] = useState({
+    name: false,
+    email: false,
+    message: false,
+  });
+
+  const [confirmationMessage, setConfirmationMessage] = useState(null);
+
+  const form = useRef();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleBlur = (field) => {
+    setTouchedFields((prevTouchedFields) => ({
+      ...prevTouchedFields,
+      [field]: true,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // Validate form fields
+    const isFormValid = validateForm();
+
+    if (isFormValid) {
+      // Use emailjs for sending email
+      try {
+        await emailjs.sendForm('service_wrbegna', 'template_f80tx2g', form.current);
+        setConfirmationMessage("Thank you for reaching out! Your message was sent, and I will get back to you as soon as possible.");
+      } catch (error) {
+        console.error("Error submitting form:", error);
+      }
+    }
+  };
+
+  const validateForm = () => {
+    const isNameValid = formData.name.trim() !== "";
+    const isEmailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formData.email);
+    const isMessageValid = formData.message.trim() !== "";
+
+    setTouchedFields({
+      name: true,
+      email: true,
+      message: true,
+    });
+
+    // Return true if all fields are valid
+    return isNameValid && isEmailValid && isMessageValid;
+  };
+
   return (
     <div>
       <h1>Contact</h1>
-      <form className="contact-form">
+      <form ref={form} className="contact-form" onSubmit={handleSubmit}>
         <label>
           Name:
           <input type="text" name="Name" />
@@ -17,7 +80,8 @@ export default function Contact() {
           Message:
           <textarea name="Message" rows="10"></textarea>
         </label>
-        <input type="submit" value="Submit" />
+        <button type="submit">Submit</button> {/* Replaced input with button for better clarity */}
+        {confirmationMessage && <p className="success-message">{confirmationMessage}</p>}
       </form>
     </div>
   );
